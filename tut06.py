559ff5a40382fd2aed9d66cabbf04f6b705fe23c
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2024-06-05 02:37:29 +0600

    Add files via upload

---
+++ tut06.py
@@ -0,0 +1,35 @@
+# (In Python, Everything is Object)*****
+
+a = 1 
+print(a)
+a1 = 10
+c = True
+d = None
+e = complex(8,3)
+print(e)
+
+b = "Sabbir"
+print(b)
+
+print(a+a1)
+
+# Data Type
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of a is ", type(c))
+print("The type of a is ", type(d))
+
+# Sequence Data---->list,tuple
+# list---> list of different data type (Changeable/Mutable)
+list1 = [8,"Sabbir", 2.3456, True]
+print(list1)
+
+# tuple---> list of different data type (UnChangeable/Immutable)
+tuple1 = [8,"Naimur", 2.3456, False]
+print(tuple1)
+
+# Mapped data---->Dictionary (dict)
+# Dictionary-->Key Value Players
+# Dictionary--->Mainly doing Mapping
+dict1 = {"name":"Roni", "age":24, "canVote": True}
+print(dict1)
