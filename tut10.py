ed6fe394746114aba3f63203afa39b2739b271d8
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2024-06-05 02:38:27 +0600

    Add files via upload

---
+++ tut10.py
@@ -0,0 +1,9 @@
+# Taking User Input
+
+a = input("Enter your name: ")
+print("My name is ",a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print(x + y)
+print("Sum: ",(int(x) + int(y)))
