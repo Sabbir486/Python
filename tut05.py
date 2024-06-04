9cd40ac919461cd620d731fe12843393476915c1
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2024-06-05 02:37:13 +0600

    Add files via upload

---
+++ tut05.py
@@ -0,0 +1,10 @@
+# Escape Sequence Character
+# (\" \") to keep something into double qutation we use this syntax
+print("I am a \"Good Boy\" ")
+print("and he also a Good Boy") 
+
+# We can keep multiple value in print statement
+print("Hey", 6 , 7)
+
+# To join multiple character we can use (sep="~")
+print("Hey", 6 , 7, sep="~", end="356")
