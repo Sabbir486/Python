1021e9e59ce9c67072ef4cacbcdebbff46138814
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2024-06-05 02:37:55 +0600

    Add files via upload

---
+++ tut08.py
@@ -0,0 +1,10 @@
+# Creating a Calculator:
+# Addition, Subtraction, Multiplication, Division
+print("********Calculator********")
+x = 5
+y = 10
+
+print("Addition: ",(x+y))
+print("Subtraction: ",(x-y))
+print("Multiplication: ",(x*y))
+print("Division: ",(x/y))
