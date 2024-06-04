079ff4dccc3c86e511964632a33b272e56062436
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2024-06-05 02:37:40 +0600

    Add files via upload

---
+++ tut07.py
@@ -0,0 +1,12 @@
+# Operators
+# Addition, Subtraction, Multiplication, Division, Modulus, Floor Division,Exponential
+
+print(5+6)
+print(5-6)
+print(5*6)
+print(5/6)
+# Floor Division
+print(5//6)
+print(5%6)
+# Exponential
+print(5**6)
