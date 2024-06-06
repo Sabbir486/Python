771191a39232c145321b7e8d48a9c23dcfb9262c
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2024-06-07 02:09:06 +0600

    Add files via upload

---
+++ tut12.py
@@ -0,0 +1,17 @@
+# String Slicing 7 Operations
+
+names = "Sabbir, Roni"
+
+print(len(names))
+print(names[0:5])
+print(names[:5])
+print(names[1:5])
+print(names[1:])
+print(names[1:-2])
+# print(names[-1:-2]) #Don't show anything
+print()
+print(names[-3:-1]) #Complicated,Don't need to use this
+
+# Quiz--->
+nm = "Harry"
+print(nm[len(nm)-4:len(nm)-2])
