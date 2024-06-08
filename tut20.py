5d1cecbe1b8c3aca4be5762fc5d95a7625d20972
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2024-06-09 01:28:49 +0600

    Add files via upload

---
+++ tut20.py
@@ -0,0 +1,36 @@
+# Functions---->
+
+# def geomean(a,b):
+#     mean = (a*b)/(a+b)
+#     print(mean)
+
+# c= 21
+# d= 20
+# geomean(c,d)
+
+# if(c>d):
+#     print('c is greater')
+# else:
+#     print('d is greater')
+
+# For user defined function we use (def) keyword
+
+def geomean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+def isGreater(a,b):
+    if(a>b):
+        print(a," is greater")
+    else:
+        print(b," is greater")
+
+def isLesser(a, b):
+    pass
+
+
+c= 21
+d= 20
+
+geomean(c,d)
+isGreater(c,d)
