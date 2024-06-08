37b2108ae2bac1fae2331fc1faa54e47ea8beb5d
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2024-06-09 01:27:54 +0600

    Add files via upload

---
+++ tut17.py
@@ -0,0 +1,24 @@
+# For Loop--->
+
+name = 'sabbir'
+for i in name:
+    print(i)
+    if(i == 'b'):
+        print("This is something special")
+print()
+
+colors = ['red', 'green', 'blue']
+for x in colors:
+    print(x)
+    for i in x:
+        print(i)
+print()
+
+# Range
+        
+for k in range(5):
+    print(k)
+print()
+
+for y in range(1,9):
+    print(y)
