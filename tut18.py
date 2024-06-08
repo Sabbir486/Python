a422a2508f4e5cf5fc4f0e33700f61bd994384a9
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2024-06-09 01:28:11 +0600

    Add files via upload

---
+++ tut18.py
@@ -0,0 +1,15 @@
+# While Loop---->
+
+i = int(input('Enter the number: '))
+while(i<=10):
+    i = int(input('Print the number: '))
+    print(i)
+
+print("Done")
+
+count = 5
+while(count>0):
+    print(count)
+    count = count-1
+else:
+    print("I am happy now")
