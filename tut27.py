e18290baa3d2c61481baddc5962b5b9c6a5467f4
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2024-06-11 01:35:23 +0600

    Add files via upload

---
+++ tut27.py
@@ -0,0 +1,23 @@
+# Quiz----> Kon Banega Crorepati....????
+
+print("To start the game write Start in your response")
+response = input("Enter Your Response: ")
+
+if(response == "Start"):
+    print("Start the Game")
+    print("Your first question: What is your name??")
+    option = "Sabbir"
+    option = "Roni"
+    option = ("Your ans: \n")
+    
+    input(option)
+    if(option == "Roni"):
+        print("Your Ans is Correct")
+        print("Next Qustion: How old are you?")
+    else:
+        print('Your ans is not correct')
+    
+
+else:
+    print("Wait for a few second")
+
